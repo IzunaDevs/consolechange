0754682e8a45d4b9dd4a129fca0c218663bed268
AraHaan <seandhunt_7@yahoo.com>
2017-09-18 14:18:37 -0400

    Change C code.

--- consolechange/_consolechange.c
+++ consolechange/_consolechange.c
@@ -10,27 +10,29 @@ consoletitle(PyObject *self, PyObject *args) {
   const char *title;
   if (!PyArg_ParseTuple(args, "s", &title))
       return NULL;
-  _change_title(title);
+#if defined(_WIN32)
+  SetConsoleTitleA(title);
+#elif defined(__linux__) || defined(__CYGWIN__)
+  printf("\033]2;%s\007\n\033[1A", title);
+#else
+  printf("\033]0;%s\007", title);
+#endif
   Py_RETURN_NONE;
 }
 
 static PyObject *
 consolesize(PyObject *self, PyObject *args) {
-  return _change_size(args);
+#if defined(_WIN32)
+  int rows, cols;
+  if (!PyArg_ParseTuple(args, "ii", &rows, &cols))
+      return NULL;
+  char buffer [256];
+  snprintf(buffer, sizeof(buffer),
+    "mode con: cols=%i lines=%i", rows, cols);
+  system(buffer);
+#endif
+  Py_RETURN_NONE;
 }
-
-static PyMethodDef _consolechange_methods[] = {
-  {"consoletitle", consoletitle, METH_VARARGS,
-   CONSOLETITLE__DOC__},
-  {"consolesize", consolesize, METH_VARARGS,
-   CONSOLESIZE__DOC__},
-  {NULL, NULL, 0, NULL}
-};
-
-static struct PyModuleDef _consolechangemodule = {
-  PyModuleDef_HEAD_INIT, "_consolechange",
-  _CONSOLECHANGE__DOC__, -1, _consolechange_methods
-};
 
 PyMODINIT_FUNC
 PyInit__consolechange(void)
