6f45c5e9b45b8402f318c21c4ecd4603e944c984
AraHaan <seandhunt_7@yahoo.com>
2017-11-23 18:48:56 -0500

    Update _consolechange.c

--- consolechange/_consolechange.c
+++ consolechange/_consolechange.c
@@ -1,4 +1,8 @@
-#include "_consolechange.c.h"
+#include <Python.h>
+#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 static PyObject *
 consoletitle(PyObject *self, PyObject *args) {
@@ -27,4 +31,38 @@ consolesize(PyObject *self, PyObject *args) {
   system(buffer);
 #endif
   Py_RETURN_NONE;
+}
+
+static PyMethodDef _consolechange_methods[] = {
+  {"consoletitle", consoletitle, METH_VARARGS,
+   "Sets the console (terminal) title in "
+   "a platform independent way."},
+  {"consolesize", consolesize, METH_VARARGS,
+   "Sets the console size. On Windows "
+   "this resizes the console, on any "
+   "other platforms it just returns None."},
+  {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef _consolechangemodule = {
+  PyModuleDef_HEAD_INIT, "_consolechange",
+  "Python extension module that automates"
+  " the console (or terminal) title change"
+  " stuff for you that works for most (if "
+  "not all) platforms in an single python "
+  "function (Unlike the multiple ways "
+  "people can do it already in python that"
+  " requires using sys.platform to get if "
+  "it is windows or not just to set it). "
+  "This is useful as then it allows much "
+  "simpler and platform independent code "
+  "without the need to use sys.platform "
+  "for any of it just to set an console "
+  "(Terminal in unix) title.", -1,
+  _consolechange_methods
+};
+
+PyMODINIT_FUNC
+PyInit__consolechange(void) {
+  return PyModule_Create(&_consolechangemodule);
 }
