4f95aec4848222e34abfc3c3476560630bc202b4
martmists <mail@martmists.com>
2018-01-14 22:45:02 +0100

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+import requests
+
+import nsfw_dl
+
+
+un = os.environ["PYPI_USERNAME"]
+pw = os.environ["PYPI_PASSWORD"]
+
+try:
+    remote_ver = requests.get(
+        "https://pypi.python.org/pypi/nsfw_dl/json").json()["info"]["version"]
+
+except:  # noqa pylint: disable=bare-except
+    remote_ver = "0.0.0"
+
+ver = nsfw_dl.__version__
+
+if ver != remote_ver:
+    p1 = subprocess.Popen(
+        "python3.6 setup.py sdist bdist_egg bdist_wheel".split())
+    p1.wait()
+    p2 = subprocess.Popen(
+        f"twine upload dist/*{ver}* -u {un} -p {pw}".split())
+    p2.wait()
